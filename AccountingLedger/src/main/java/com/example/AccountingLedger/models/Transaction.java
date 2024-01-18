f73de4fad5402dc18653a4a4820970c0a60447f0
velesnic <leslieeevega@gmail.com>
2024-01-18 12:31:30 -0600

    transaction class

--- AccountingLedger/src/main/java/com/example/AccountingLedger/models/Transaction.java
+++ AccountingLedger/src/main/java/com/example/AccountingLedger/models/Transaction.java
@@ -1,4 +1,35 @@
 package com.example.AccountingLedger.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "transactions")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Transaction {
+
+    //for transaction id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 15)
+    private String type;
+    @Column(nullable = false)
+    private BigDecimal amount;
+    @Column(nullable = false, length = 255)
+    private String description;
+    @Column(nullable = false)
+    private LocalDate date;
+    @Column(nullable = false)
+    private LocalTime time;
+
 }
